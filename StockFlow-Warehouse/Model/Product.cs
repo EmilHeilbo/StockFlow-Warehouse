b114e18e703341ca2b421188a4d66b7cac422d69
Emil Heilbo <Emil.Heilbo@specialisterne.dk>
2026-03-16 10:40:53 +0100

    fix: add price, barcode, and description properties to `Model.Product`
    Signed-off-by: Emil Heilbo <Emil.Heilbo@specialisterne.dk>

--- StockFlow-Warehouse/Model/Product.cs
+++ StockFlow-Warehouse/Model/Product.cs
@@ -14,6 +14,13 @@ public class Product
     public Guid Id { get; init; } = Guid.NewGuid();
     [MaxLength(100)]
     public required string Name { get; set; }
+
+    public decimal Price { get; set; } = 0;
+    [MaxLength(14)]
+    [RegularExpression("^[0-9]{0,14}$")]
+    public string Barcode { get; set; } = "";
+    [MaxLength(1200)]
+    public string Description { get; set; } = "";
     public List<Category> Categories { get; set; } = [];
 }
 
