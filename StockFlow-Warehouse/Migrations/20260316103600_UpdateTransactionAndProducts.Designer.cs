a88025d321ae236664de1bdc99e2b365eea365db
Emil Heilbo <Emil.Heilbo@specialisterne.dk>
2026-03-16 11:36:36 +0100

    feat(Transaction, Product): update schema with new properties and type changes
    Signed-off-by: Emil Heilbo <Emil.Heilbo@specialisterne.dk>

--- StockFlow-Warehouse/Migrations/20260316103600_UpdateTransactionAndProducts.Designer.cs
+++ StockFlow-Warehouse/Migrations/20260316103600_UpdateTransactionAndProducts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StockFlow_Warehouse.Model;
 
@@ -11,9 +12,11 @@ using StockFlow_Warehouse.Model;
 namespace StockFlow_Warehouse.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260316103600_UpdateTransactionAndProducts")]
+    partial class UpdateTransactionAndProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
