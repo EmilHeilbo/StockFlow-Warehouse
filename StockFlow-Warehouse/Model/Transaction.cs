9543244baaf0abd875a88878b55367e6d9b7b76b
Emil Heilbo <Emil.Heilbo@specialisterne.dk>
2026-03-16 10:37:49 +0100

    feat: add tracking number and transaction state to `Model.Transaction`
    Signed-off-by: Emil Heilbo <Emil.Heilbo@specialisterne.dk>

--- StockFlow-Warehouse/Model/Transaction.cs
+++ StockFlow-Warehouse/Model/Transaction.cs
@@ -1,4 +1,6 @@
-﻿namespace StockFlow_Warehouse.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockFlow_Warehouse.Model;
 
 public enum TransactionType
 {
@@ -8,11 +10,24 @@ public enum TransactionType
     Move
 }
 
+public enum TransactionState
+{
+    Reserved,
+    InTransit,
+    Delivered,
+    Cancelled,
+    Returned
+}
+
 public class Transaction
 {
     public Guid Id { get; init; } = Guid.NewGuid();
+    // Consider inferring Type from type of From & To fields
     public required TransactionType Type { get; init; }
+    public TransactionState State { get; set; } = TransactionState.Reserved;
     public required Recipient From { get; init; }
     public required Recipient To { get; init; }
     public List<ProductAmount> Products { get; init; } = [];
+    [MaxLength(50)]
+    public string TrackingId { get; set; } = "";
 };
